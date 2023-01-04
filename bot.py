17475b04801a2004039610dc37972f990758783b
Maxim Pinyaguin <88331457+Ozurexus@users.noreply.github.com>
2023-01-04 21:10:33 +0300

    working list commands

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from config_reader import config
+from aiogram.dispatcher.filters import CommandObject
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
@@ -12,7 +13,7 @@ mylist = []
 
 
 async def cmd_start(message: types.Message):
-    await message.answer("Hiiiii! ^_^")
+    await message.answer("Hi I'm HelperBot\nPowered by aiogram.")
 
 
 async def cmd_dice(message: types.Message):
@@ -27,29 +28,50 @@ async def cmd_test2(message: types.Message):
     await message.reply("Test 2")
 
 
-@dp.message(commands=["create_list"])
-async def cmd_create_list(message: types.Message):
+@dp.message(commands=["create"])
+async def cmd_create(message: types.Message):
     mylist = []
     await message.answer("Список создан")
 
 
-@dp.message(commands=["add_to_list"])
-async def cmd_add_to_list(message: types.Message, number: int):
-    mylist.append(number)
-    await message.answer("Добавлено число", number)
+@dp.message(commands=["add"])
+async def cmd_add(message: types.Message, command: CommandObject):
+    if command.args:
+        mylist.append(command.args)
+        await message.answer("Добавлено число " + command.args + " в список")
+    else:
+        await message.answer("Пожалуйста, укажи число после команды /add")
 
 
-@dp.message(commands=["show_list"])
-async def cmd_show_list(message: types.Message):
+@dp.message(commands=["show"])
+async def cmd_show(message: types.Message):
     await message.answer(f"Ваш список: {mylist}")
 
 
-@dp.message(commands=["clear_list"])
-async def cmd_clear_list(message: types.Message):
+@dp.message(commands=["remove"])
+async def cmd_remove(message: types.Message, command: CommandObject):
+    if command.args and command.args in mylist:
+        mylist.remove(command.args)
+        await message.answer("Удалено число " + command.args + " из списка")
+    else:
+        if command.args:
+            await message.answer("Число " + command.args + " не найдено в списке")
+        else:
+            await message.answer("Пожалуйста, укажи число после команды /remove")
+
+
+@dp.message(commands=["name"])
+async def cmd_name(message: types.Message, command: CommandObject):
+    if command.args:
+        await message.answer("Привет, " + command.args+"!")
+    else:
+        await message.answer("Пожалуйста, укажи своё имя после команды /name")
+
+
+@dp.message(commands=["clear"])
+async def cmd_clear(message: types.Message):
     mylist.clear()
     await message.answer("Список очищен")
-
-# Запуск процесса поллинга новых апдейтов
 
 
 async def main():
