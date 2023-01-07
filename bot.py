3cac2208875be89b542398bb6f1cb574b555ba1c
Maxim Pinyaguin <88331457+Ozurexus@users.noreply.github.com>
2023-01-07 19:30:03 +0300

    Buttons finished and test case

--- bot.py
+++ bot.py
@@ -6,15 +6,22 @@ from aiogram import Bot, Dispatcher, types, Router
 from aiogram.types import Message
 from aiogram.dispatcher.filters import CommandObject, Text
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-
+from datetime import datetime, timedelta
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
 dp = Dispatcher()
 mylist = []
 router = Router()
-
-
+t = [["Monday", "9:00", "303", "Shilov"],
+     ["Monday", "10:40", "108", "Zouev"],
+     ["Tuesday", "12:00", "303", "Shilov"],
+     ["Saturday", "14:00", "319", "Gorodetskiy"],
+     ["Saturday", "21:00", "303", "Shilov"],
+     ["Sunday", "10:00", "319", "Gorodetskiy"],
+     ["Sunday", "12:00", "303", "Shilov"]]
+weekdays = ["Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday", "Sunday"]
 # @dp.message(commands="start")
 # async def cmd_start(message: types.Message):
 #     kb = [
@@ -39,14 +46,26 @@ router = Router()
 # @dp.message(Text(text="TOMORROW"))
 # async def cmd_tmrw(message: types.Message):
 #     await message.reply("Зуев", reply_markup=types.ReplyKeyboardRemove())
+@ dp.message(commands=["date"])
+async def cmd_date(message: types.Message):
+    newdate = datetime.now()
+    newdate = newdate.strftime("%d.%m.%Y")
+    await message.answer("Сегодня " + newdate)
 
 
-@dp.message(commands=["dice"])
+@ dp.message(commands=["time"])
+async def cmd_time(message: types.Message):
+    newtime = datetime.now()
+    newtime = newtime.strftime("%H:%M:%S")
+    await message.answer("Сейчас " + newtime)
+
+
+@ dp.message(commands=["dice"])
 async def cmd_dice(message: types.Message):
     await message.answer_dice(emoji="🎲")
 
 
-@dp.message(commands=["random"])
+@ dp.message(commands=["random"])
 async def cmd_random(message: types.Message):
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
@@ -59,22 +78,22 @@ async def cmd_random(message: types.Message):
     )
 
 
-@dp.message(commands=["now"])
+@ dp.message(commands=["now"])
 async def cmd_now(message: types.Message):
     builder = InlineKeyboardBuilder()
-    if message.from_user.id in (1, 1847234646):
-        builder.add(types.InlineKeyboardButton(text="NOW", callback_data="now_value"),
-                    types.InlineKeyboardButton(
-                    text="TODAY", callback_data="today_value"),
-                    types.InlineKeyboardButton(
-                    text="TOMORROW", callback_data="tomorrow_value"),
-                    )
-        await message.answer(
-            "Click the button",
-            reply_markup=builder.as_markup()
-        )
-    else:
-        await message.answer("You are not me >:(")
+    # if message.from_user.id in (1, 1847234646):
+    builder.add(types.InlineKeyboardButton(text="NEXT", callback_data="next_value"),
+                types.InlineKeyboardButton(
+                text="TODAY", callback_data="today_value"),
+                types.InlineKeyboardButton(
+                text="TOMORROW", callback_data="tomorrow_value"),
+                )
+    await message.answer(
+        "Click the button",
+        reply_markup=builder.as_markup()
+    )
+    # else:
+    #     await message.answer("You are not me >:(")
 
 
 @dp.callback_query(text="random_value")
@@ -83,25 +102,57 @@ async def send_random_value(callback: types.CallbackQuery):
     await callback.answer()
 
 
-@dp.callback_query(text="now_value")
-async def send_random_value(callback: types.CallbackQuery):
-    await callback.message.answer("Шилов")
+@dp.callback_query(text="next_value")
+async def send_next_value(callback: types.CallbackQuery):
+    newdate = datetime.now()
     # await callback.answer(
     #     text="Спасибо, что воспользовались ботом!",
     #     show_alert=True
     # )
+    pairs = False
+    for i in range(len(t)):
+        if newdate.strftime("%A") == t[i][0] and newdate.strftime("%H:%M") < t[i][1]:
+            pairs = True
+            await callback.message.answer(t[i][0] + "\n" + t[i][1] + "\n" + t[i][2] + "\n" + t[i][3])
+            break
+    if pairs == False:
+        await callback.message.answer("No classes left 🎉")
     await callback.answer()
 
 
 @dp.callback_query(text="today_value")
-async def send_random_value(callback: types.CallbackQuery):
-    await callback.message.answer("Шилов + Городецкий")
+async def send_today_value(callback: types.CallbackQuery):
+    today_string = ""
+    newdate = datetime.now()
+    for i in range(len(t)):
+        if newdate.strftime("%A") == t[i][0]:
+            today_string += t[i][0] + "\n" + t[i][1] + \
+                "\n" + t[i][2] + "\n" + t[i][3] + "\n\n"
+    if today_string == "":
+        await callback.message.answer("No classes today 🎉")
+    else:
+        await callback.message.answer(today_string)
     await callback.answer()
 
 
 @dp.callback_query(text="tomorrow_value")
-async def send_random_value(callback: types.CallbackQuery):
-    await callback.message.answer("Зуев")
+async def send_tmrw_value(callback: types.CallbackQuery):
+    tomorrow_string = ""
+    newdate = datetime.now()
+    tmrw = ""
+    for i in range(len(weekdays)):
+        if newdate.strftime("%A") == weekdays[i] and i == len(weekdays)-1:
+            tmrw = weekdays[0]
+        elif newdate.strftime("%A") == weekdays[i]:
+            tmrw = weekdays[i+1]
+    for i in range(len(t)):
+        if tmrw == t[i][0]:
+            tomorrow_string += t[i][0] + "\n" + t[i][1] + \
+                "\n" + t[i][2] + "\n" + t[i][3] + "\n\n"
+    if tomorrow_string == "":
+        await callback.message.answer("No classes tomorrow 🎉")
+    else:
+        await callback.message.answer(tomorrow_string)
     await callback.answer()
 
 
@@ -161,9 +212,10 @@ async def cmd_clear(message: types.Message):
     await message.answer("Список очищен")
 
 
-# @dp.message(content_types="text")
-# async def echo(message: types.Message):
-#     await message.answer(message.text)
+@dp.message(content_types="text")
+async def echo(message: types.Message):
+    print("\n", message.text, "\n")
+    await message.answer(message.text)
 # async def extract_data(message: types.Message):
 #     data = {
 #         "url": "<N/A>",
