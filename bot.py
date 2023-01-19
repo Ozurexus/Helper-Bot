fc811775886a8ef6bed455d4a5a1f8592f18087d
Maxim Pinyaguin <88331457+Ozurexus@users.noreply.github.com>
2023-01-19 12:50:32 +0300

    added week schedule

--- bot.py
+++ bot.py
@@ -7,11 +7,11 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.dispatcher.filters import CommandObject, Text
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.client.session.aiohttp import AiohttpSession
-# from aiogram.methods import send_location
+from contextlib import suppress
+from aiogram.exceptions import TelegramBadRequest
+
 f = open("info.txt", "r")
 array = f.readlines()
-counter = 0
-prev_message_id = 0
 LOGIN = str(array[0].rstrip())
 PASSWORD = str(array[1].rstrip())
 PROXY_URL = str(array[2].rstrip())
@@ -25,9 +25,10 @@ session = AiohttpSession(proxy=(PROXY_URL, auth))
 bot = Bot(token=BOT_TOKEN, session=session)
 
 # bot = Bot(token=BOT_TOKEN)
+
 dp = Dispatcher()
-owm = OWM(API_KEY)
-mylist = []
+user_data = {}
+users = []
 table = [["Monday", "9:20", "10:50", "ONLINE", "Adil Khan",
           "Introduction to Machine Learning (lec)"],
          ["Monday", "13:00", "14:30", "317", "Roman Garaev",
@@ -52,9 +53,6 @@ table = [["Monday", "9:20", "10:50", "ONLINE", "Adil Khan",
 weekdays = ["Monday", "Tuesday", "Wednesday",
             "Thursday", "Friday", "Saturday", "Sunday"]
 
-user_data = {}
-users = []
-
 
 def get_keyboard():
     buttons = [[
@@ -62,14 +60,27 @@ def get_keyboard():
         types.InlineKeyboardButton(
             text="TODAY", callback_data="num_today"),
         types.InlineKeyboardButton(
-            text="TOMORROW", callback_data="num_tomorrow"),
-        types.InlineKeyboardButton(text="WEEK", callback_data="num_week"),]]
+            text="TOMORROW", callback_data="num_tomorrow"),]]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
+
+
+def get_keyboard2():
+    buttons = [[types.InlineKeyboardButton(text="MONDAY", callback_data="monday_value"),
+                types.InlineKeyboardButton(
+                    text="TUESDAY", callback_data="tuesday_value"),
+                types.InlineKeyboardButton(text="WEDNESDAY", callback_data="wednesday_value")],
+               [types.InlineKeyboardButton(text="THURSDAY", callback_data="thursday_value"),
+                types.InlineKeyboardButton(
+                    text="FRIDAY", callback_data="friday_value"),
+                types.InlineKeyboardButton(text="SATURDAY", callback_data="saturday_value")]]
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
     return keyboard
 
 
 async def update_num_text(message: types.Message, new_value: int):
-    await message.edit_text(new_value, reply_markup=get_keyboard())
+    with suppress(TelegramBadRequest):
+        await message.edit_text(new_value, reply_markup=get_keyboard())
 
 
 @dp.message(commands=["start"])
@@ -96,7 +107,6 @@ async def cmd_show(message: types.Message):
 
 @dp.callback_query(Text(text_startswith="num_"))
 async def callbacks_num(callback: types.CallbackQuery):
-    user_value = user_data.get(callback.from_user.id, 0)
     action = callback.data.split("_")[1]
     if action == "next":
         user_data[callback.from_user.id] = get_next()
@@ -104,8 +114,6 @@ async def callbacks_num(callback: types.CallbackQuery):
         user_data[callback.from_user.id] = get_today()
     elif action == "tomorrow":
         user_data[callback.from_user.id] = get_tomorrow()
-    elif action == "week":
-        user_data[callback.from_user.id] = "NO LOL"
     await update_num_text(callback.message, user_data[callback.from_user.id])
     await callback.answer()
 
@@ -218,159 +226,150 @@ def get_tomorrow():
     return msg
 
 
-@dp.callback_query(text="choose_value")
-async def send_choose_value(callback: types.CallbackQuery):
-    builder = InlineKeyboardBuilder()
-    builder.add(types.InlineKeyboardButton(
-        text="MON", callback_data="monday_value"),
-        types.InlineKeyboardButton(
-        text="TUE", callback_data="tuesday_value"),
-        types.InlineKeyboardButton(
-        text="WED", callback_data="wednesday_value"),
-        types.InlineKeyboardButton(
-        text="THUR", callback_data="thursday_value"),
-        types.InlineKeyboardButton(
-        text="FRI", callback_data="friday_value"),
-    )
-    await callback.message.answer("Choose a weekday:", reply_markup=builder.as_markup())
+@dp.message(commands=["week"])
+async def cmd_week(message: types.Message):
+    await message.answer("Choose a weekday:", reply_markup=get_keyboard2())
 
 
-@dp.callback_query(text="monday_value")
+@ dp.callback_query(text="monday_value")
 async def send_monday_value(callback: types.CallbackQuery):
-    monday_string = ""
+    msg = ""
     weekday = False
     for i in range(len(table)):
         if "Monday" == table[i][0]:
             if weekday == False:
-                monday_string += table[i][0]+":\n"
+                msg += table[i][0]+":\n"
                 weekday = True
-            monday_string += table[i][5]+"\n" + \
-                table[i][4]+"\n"+table[i][3]+"\n" + \
-                table[i][1]+" - "+table[i][2]+"\n\n"
-    monday_string += "/start"
-    if monday_string == "/start":
+            msg += table[i][5]+"\n" + table[i][4]+"\n" + \
+                table[i][3]+"\n" + table[i][1]+" - "+table[i][2]+"\n\n"
+    if msg == "":
         await callback.message.answer("No classes on Monday 🎉")
     else:
-        await callback.message.answer(monday_string)
+        await callback.message.answer(msg)
     await callback.answer()
 
 
-@dp.callback_query(text="tuesday_value")
+@ dp.callback_query(text="tuesday_value")
 async def send_tuesday_value(callback: types.CallbackQuery):
-    tuesday_string = ""
+    msg = ""
     weekday = False
     for i in range(len(table)):
         if "Tuesday" == table[i][0]:
             if weekday == False:
-                tuesday_string += table[i][0]+":\n"
+                msg += table[i][0]+":\n"
                 weekday = True
-            tuesday_string += table[i][5]+"\n" + \
-                table[i][4]+"\n"+table[i][3]+"\n" + \
-                table[i][1]+" - "+table[i][2]+"\n\n"
-    tuesday_string += "/start"
-    if tuesday_string == "/start":
+            msg += table[i][5]+"\n" + table[i][4]+"\n" + \
+                table[i][3]+"\n" + table[i][1]+" - "+table[i][2]+"\n\n"
+    if msg == "":
         await callback.message.answer("No classes on Tuesday 🎉")
     else:
-        await callback.message.answer(tuesday_string)
+        await callback.message.answer(msg)
     await callback.answer()
 
 
-@dp.callback_query(text="wednesday_value")
+@ dp.callback_query(text="wednesday_value")
 async def send_wednesday_value(callback: types.CallbackQuery):
-    wednesday_string = ""
+    msg = ""
     weekday = False
     for i in range(len(table)):
         if "Wednesday" == table[i][0]:
             if weekday == False:
-                wednesday_string += table[i][0]+":\n"
+                msg += table[i][0]+":\n"
                 weekday = True
-            wednesday_string += table[i][5]+"\n" + \
-                table[i][4]+"\n"+table[i][3]+"\n" + \
-                table[i][1]+" - "+table[i][2]+"\n\n"
-    wednesday_string += "/start"
-    if wednesday_string == "/start":
+            msg += table[i][5]+"\n" + table[i][4]+"\n" + \
+                table[i][3]+"\n" + table[i][1]+" - "+table[i][2]+"\n\n"
+    if msg == "":
         await callback.message.answer("No classes on Wednesday 🎉")
     else:
-        await callback.message.answer(wednesday_string)
+        await callback.message.answer(msg)
     await callback.answer()
 
 
-@dp.callback_query(text="thursday_value")
+@ dp.callback_query(text="thursday_value")
 async def send_thursday_value(callback: types.CallbackQuery):
-    thursday_string = ""
+    msg = ""
     weekday = False
     for i in range(len(table)):
         if "Thursday" == table[i][0]:
             if weekday == False:
-                thursday_string += table[i][0]+":\n"
+                msg += table[i][0]+":\n"
                 weekday = True
-            thursday_string += table[i][5]+"\n" + \
-                table[i][4]+"\n"+table[i][3]+"\n" + \
-                table[i][1]+" - "+table[i][2]+"\n\n"
-    thursday_string += "/start"
-    if thursday_string == "/start":
+            msg += table[i][5]+"\n" + table[i][4]+"\n" + \
+                table[i][3]+"\n" + table[i][1]+" - "+table[i][2]+"\n\n"
+    if msg == "":
         await callback.message.answer("No classes on Thursday 🎉")
     else:
-        await callback.message.answer(thursday_string)
+        await callback.message.answer(msg)
     await callback.answer()
 
 
-@dp.callback_query(text="friday_value")
+@ dp.callback_query(text="friday_value")
 async def send_friday_value(callback: types.CallbackQuery):
-    friday_string = ""
+    msg = ""
     weekday = False
     for i in range(len(table)):
         if "Friday" == table[i][0]:
             if weekday == False:
-                friday_string += table[i][0]+":\n"
+                msg += table[i][0]+":\n"
                 weekday = True
-            friday_string += table[i][5]+"\n" + \
-                table[i][4]+"\n"+table[i][3]+"\n" + \
-                table[i][1]+" - "+table[i][2]+"\n\n"
-    friday_string += "/start"
-    if friday_string == "/start":
+            msg += table[i][5]+"\n" + table[i][4]+"\n" + \
+                table[i][3]+"\n" + table[i][1]+" - "+table[i][2]+"\n\n"
+    if msg == "":
         await callback.message.answer("No classes on Friday 🎉")
     else:
-        await callback.message.answer(friday_string)
+        await callback.message.answer(msg)
     await callback.answer()
 
 
-@dp.message(commands=["delete"])
+@ dp.callback_query(text="saturday_value")
+async def send_saturday_value(callback: types.CallbackQuery):
+    msg = ""
+    weekday = False
+    for i in range(len(table)):
+        if "Saturday" == table[i][0]:
+            if weekday == False:
+                msg += table[i][0]+":\n"
+                weekday = True
+            msg += table[i][5]+"\n" + table[i][4]+"\n" + \
+                table[i][3]+"\n" + table[i][1]+" - "+table[i][2]+"\n\n"
+    if msg == "":
+        await callback.message.answer("No classes on Saturday 🎉")
+    else:
+        await callback.message.answer(msg)
+    await callback.answer()
+
+
+@ dp.message(commands=["delete"])
 async def cmd_delete(message: types.Message):
     await bot.delete_message(message.chat.id, message.message_id)
 
 
-@dp.message(commands=["date"])
+@ dp.message(commands=["date"])
 async def cmd_date(message: types.Message):
     newdate = datetime.now()+timedelta(hours=3)
     newdate = newdate.strftime("%d.%m.%Y")
     await message.answer("Today is " + newdate)
 
 
-@dp.message(commands=["time"])
+@ dp.message(commands=["time"])
 async def cmd_time(message: types.Message):
     newtime = datetime.now()+timedelta(hours=3)
     newtime = newtime.strftime("%H:%M:%S")
     await message.answer("Now is " + newtime)
 
 
-@dp.message(commands=["dice"])
+@ dp.message(commands=["dice"])
 async def cmd_dice(message: types.Message):
     await message.answer_dice(emoji="🎲")
 
 
-@dp.message(commands=["id"])
+@ dp.message(commands=["id"])
 async def cmd_id(message: types.Message):
     msg = "Your id is "+str(message.from_user.id)
     await message.answer(msg)
 
 
-@dp.message(commands=["test"])
-async def cmd_test1(message: types.Message):
-    await message.reply("Test")
-
-
-@dp.message(commands=["name"])
+@ dp.message(commands=["name"])
 async def cmd_name(message: types.Message, command: CommandObject):
     if command.args:
         await message.answer("Hello, " + command.args+"!")
@@ -378,9 +377,10 @@ async def cmd_name(message: types.Message, command: CommandObject):
         await message.answer("Please write your name after /name")
 
 
-@dp.message(commands=["weather"])
+@ dp.message(commands=["weather"])
 async def cmd_weather(message: types.Message, command: CommandObject):
     if command.args:
+        owm = OWM(API_KEY)
         mgr = owm.weather_manager()
         observation = mgr.weather_at_place(command.args)
         w = observation.weather
@@ -390,10 +390,11 @@ async def cmd_weather(message: types.Message, command: CommandObject):
         await message.answer("Please write your city after /weather")
 
 
-@dp.message(content_types=['location'])
+@ dp.message(content_types=['location'])
 async def handle_location(message: types.Message):
     lat = message.location.latitude
     lon = message.location.longitude
+    owm = OWM(API_KEY)
     mgr = owm.weather_manager()
     observation = mgr.weather_at_coords(lat, lon)
     w = observation.weather
@@ -401,23 +402,18 @@ async def handle_location(message: types.Message):
     await message.answer(f"Air temperature at latitude: {lat},longitude: {lon}: \n{temp}°C")
 
 
-# @ dp.message(content_types=['sendloco'])
-# async def handle_location(message: types.Message):
-#     pass
-
-
-@dp.message(content_types="text")
+@ dp.message(content_types="text")
 async def echo(message: types.Message):
     print("\n", message.text, "\n")
     await message.answer("I don't understand you")
 
 
-@dp.message(content_types=[types.ContentType.ANIMATION])
+@ dp.message(content_types=[types.ContentType.ANIMATION])
 async def echo_gif(message: types.Message):
     await message.reply_animation(message.animation.file_id)
 
 
-@dp.message(content_types=[types.ContentType.STICKER])
+@ dp.message(content_types=[types.ContentType.STICKER])
 async def echo_sticker(message: types.Message):
     await message.reply_sticker(message.sticker.file_id)
 
